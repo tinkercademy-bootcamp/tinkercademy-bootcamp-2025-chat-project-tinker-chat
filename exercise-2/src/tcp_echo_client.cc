8e0517a1b511b6af2db103234267a20ed51836c0
harshit kansal <harshitkansal25@gmail.com>
2025-05-26 11:54:26 +0000

    tried to split the send_and_message function to a different send and message function and call them in main separately

--- exercise-2/src/tcp_echo_client.cc
+++ exercise-2/src/tcp_echo_client.cc
@@ -40,17 +40,16 @@ void connect_to_server(int sock, sockaddr_in &server_address) {
   }
 }
 
-void send_and_receive_message(int sock, const std::string &message) {
+void send_message(int sock , const std::string &message){
+  send(sock, message.c_str(), message.size(), 0);
+  std::cout << "Sent: " << message << "\n";
+}
+void receive_message(int sock){
+  // Receive response from the server
   const int kBufferSize = 1024;
   // #Question - is buffer the best name we can use?
   // buffer is a commonly used name for temporary store in I/O operations, it can be improved for this specific context by doing something like RcvdMsg_buffer
   char buffer[kBufferSize] = {0};
-
-  // Send the message to the server
-  send(sock, message.c_str(), message.size(), 0);
-  std::cout << "Sent: " << message << "\n";
-
-  // Receive response from the server
   ssize_t read_size = read(sock, buffer, kBufferSize);
   if (read_size > 0) {
     std::cout << "Received: " << buffer << "\n";
@@ -85,7 +84,8 @@ int main(int argc, char *argv[]) {
   sockaddr_in server_address = create_address(kServerAddress, kPort);
 
   connect_to_server(my_socket, server_address);
-  send_and_receive_message(my_socket, message);
+  send_message(my_socket , message);
+  receive_message(my_socket);
   close(my_socket);
 
   return 0;
