8f7b9d0e3773018b853d8a7526ab10c535e7653b
Shreyas Sachidananda Adiga <user@ip-172-31-25-83.ap-south-1.compute.internal>
2025-05-27 21:18:08 +0000

    Client read_args modified a bit

--- exercise-2/src/tcp_echo_client.cc
+++ exercise-2/src/tcp_echo_client.cc
@@ -61,15 +61,14 @@ void send_and_receive_message(int sock, const std::string &message) {
 }
 
 // #Question - what can be improved in this function?
+ // Redundant if condition removed
 std::string read_args(int argc, char *argv[]) {
   std::string message = "Hello from client";
-  if (argc == 1) {
+  if (argc <= 1) {
     std::cout << "Usage: " << argv[0] << " <message>\n";
     exit(EXIT_FAILURE);
   }
-  if (argc > 1) {
     message = argv[1];
-  }
   return message;
 }
 
