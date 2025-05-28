f2896d37f96232cc1d30c3fdfd5da008b0022da5
Rajasree Ponnada <user@ip-172-31-25-255.ap-south-1.compute.internal>
2025-05-28 19:07:53 +0000

    feat: restored check_error abstraction in server-main.cc

--- exercise-5/src/server-main.cc
+++ exercise-5/src/server-main.cc
@@ -3,20 +3,18 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include "../server/server.h"
 
 #include "net/chat-sockets.h"
-// #include "utils.h"
-#include "../server/server.h"
+#include "utils.h"
+
 namespace tt::chat::server {
 
 void set_socket_options(int sock, int opt) {
   namespace ttc = tt::chat;
   auto err_code = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
                              &opt, sizeof(opt));
-  if (err_code < 0) {
-    std::cerr << "setsockopt() error\n";
-    exit(EXIT_FAILURE);
-  }
+  ttc::check_error(err_code < 0, "setsockopt() error\n");
 }
 
 int create_server_socket() {
@@ -28,19 +26,13 @@ int create_server_socket() {
 void bind_address_to_socket(int sock, sockaddr_in &address) {
   namespace ttc = tt::chat;
   auto err_code = bind(sock, (sockaddr *)&address, sizeof(address));
-  if (err_code < 0) {
-    std::cerr << "bind failed\n";
-    exit(EXIT_FAILURE);
-  }
+  ttc::check_error(err_code < 0, "bind failed\n");
 }
 
 void listen_on_socket(int sock) {
   namespace ttc = tt::chat;
   auto err_code = listen(sock, 3);
-  if (err_code < 0) {
-    std::cerr << "listen failed\n";
-    exit(EXIT_FAILURE);
-  }
+  ttc::check_error(err_code < 0, "listen failed\n");
 }
 
 void handle_accept(int sock) {
@@ -48,43 +40,41 @@ void handle_accept(int sock) {
   const int kBufferSize = 1024;
   char buffer[kBufferSize] = {0};
   ssize_t read_size = read(sock, buffer, kBufferSize);
- 
-  if (read_size < 0) {
-    std::cerr << "Read error on client socket " << sock << "\n";
-    close(sock);
-    exit(EXIT_FAILURE);
-  }
+
+  ttc::check_error(read_size < 0,
+                   "Read error on client socket " + std::to_string(sock));
   if (read_size > 0) {
     std::cout << "Received:" << buffer << "\n";
     send(sock, buffer, read_size, 0);
     std::cout << "Echo message sent\n";
   } else if (read_size == 0) {
     std::cout << "Client disconnected.\n";
+  } else {
+    std::cerr << "Read error on client socket " << sock << "\n";
   }
   close(sock);
 }
- 
+
 sockaddr_in create_server_address(int port) {
   namespace ttn = tt::chat::net;
   sockaddr_in address = ttn::create_address(port);
   address.sin_addr.s_addr = INADDR_ANY;
   return address;
 }
- 
+
 void handle_connections(int sock, sockaddr_in &address) {
   socklen_t address_size = sizeof(address);
 
   while (true) {
     int accepted_socket = accept(sock, (sockaddr *)&address, &address_size);
-    if (accepted_socket < 0) {
-      std::cerr << "Accept error\n";
-      exit(EXIT_FAILURE);
-    }
+    check_error(accepted_socket < 0, "Accept error n ");
     handle_accept(accepted_socket);
   }
 }
 
 } // namespace tt::chat::server
+
+
 
 int main() {
   // namespace ttc = tt::chat;
