35aaaba00de24caf6e7302dfeb24e44db8abf767
Rishika Garg <user@ip-172-31-18-95.ap-south-1.compute.internal>
2025-06-01 16:41:49 +0000

    Refactor- Add create_server_address function

--- exercise-5/src/client/client.cpp
+++ exercise-5/src/client/client.cpp
@@ -16,4 +16,10 @@ Client::Client(const std::string& server_address, int port)
   create_server_address(server_address, port);
   connectToServer();
 }
+void Client::create_server_address(const std::string& server_ip, int port) {
+  server_address_ = net::create_address(port);
+  // Convert the server IP address to a binary format
+  auto err_code = inet_pton(AF_INET, server_ip.c_str(), &server_address_.sin_addr);
+  check_error(err_code <= 0, "Invalid address/ Address not supported\n");
+}
 }
