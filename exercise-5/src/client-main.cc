f5bb78df7d42981335f03ae733cf1cd2353cc373
Rishika Garg <user@ip-172-31-18-95.ap-south-1.compute.internal>
2025-06-01 23:12:03 +0000

    Refactor- Write the main function in client-main.cpp

--- exercise-5/src/client-main.cc
+++ exercise-5/src/client-main.cc
@@ -6,44 +6,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-
-#include "net/chat-sockets.h"
-#include "utils.h"
-
-sockaddr_in create_server_address(const std::string &server_ip, int port) {
-  using namespace tt::chat;
-  sockaddr_in address = net::create_address(port);
-  // Convert the server IP address to a binary format
-  auto err_code = inet_pton(AF_INET, server_ip.c_str(), &address.sin_addr);
-  check_error(err_code <= 0, "Invalid address/ Address not supported\n");
-  return address;
-}
-
-void connect_to_server(int sock, sockaddr_in &server_address) {
-  using namespace tt::chat;
-  auto err_code =
-      connect(sock, (sockaddr *)&server_address, sizeof(server_address));
-  check_error(err_code < 0, "Connection Failed.\n");
-}
-
-void send_and_receive_message(int sock, const std::string &message) {
-  using namespace tt::chat;
-  const int kBufferSize = 1024;
-  char recv_buffer[kBufferSize] = {0};
-
-  // Send the message to the server
-  send(sock, message.c_str(), message.size(), 0);
-  std::cout << "Sent: " << message << "\n";
-
-  // Receive response from the server
-  ssize_t read_size = read(sock, recv_buffer, kBufferSize);
-  check_error(read_size < 0, "Read error.\n");
-  if (read_size > 0) {
-    std::cout << "Received: " << recv_buffer << "\n";
-  } else if (read_size == 0) {
-    std::cout << "Server closed connection.\n";
-  }
-}
+#include "client/client.h"
 
 std::string read_args(int argc, char *argv[]) {
   using namespace tt::chat;
@@ -63,13 +26,10 @@ int main(int argc, char *argv[]) {
   const std::string kServerAddress = "127.0.0.1";
 
   std::string message = read_args(argc, argv);
+  using namespace tt::chat;
+  client::Client client(kServerAddress, kPort);
 
-  int my_socket = tt::chat::net::create_socket();
-  sockaddr_in server_address = create_server_address(kServerAddress, kPort);
-
-  connect_to_server(my_socket, server_address);
-  send_and_receive_message(my_socket, message);
-  close(my_socket);
+  client.send_and_receive_message(message);
 
   return 0;
 }
