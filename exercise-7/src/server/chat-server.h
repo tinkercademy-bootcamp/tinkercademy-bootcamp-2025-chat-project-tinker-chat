bc05ead023e9a74ea1ad82be7bf67ccab28a5f67
Rishika Garg <user@ip-172-31-18-95.ap-south-1.compute.internal>
2025-06-02 18:14:26 +0000

    Feat- Add member functions and variables for server class

--- exercise-7/src/server/chat-server.h
+++ exercise-7/src/server/chat-server.h
@@ -2,6 +2,10 @@
 #define CHAT_SERVER_H
 
 #include <netinet/in.h>
+#include <unordered_map>
+#include <set>
+#include <vector>
+#include "client-info.h"
 namespace tt::chat::server {
 
 class Server {
@@ -13,11 +17,18 @@ public:
 private:
   int socket_;
   sockaddr_in address_;
+  int epoll_fd_;
+  std::unordered_map<int, ClientInfo> clients_;
+  std::unordered_map<std::string, std::set<int>> channels_;
 
   static constexpr int kBufferSize = 1024;
 
   void handle_accept(int sock);
+  void handle_client_message(int client_fd);
+  void process_message(int client_fd, const std::string& msg);
+  void broadcast_to_channel(const std::string& channel, const std::string& msg, int sender_fd = -1);
   static void set_socket_options(int sock, int opt);
+  static int make_socket_non_blocking(int sfd);
 };
 } // namespace tt::chat::server
 
