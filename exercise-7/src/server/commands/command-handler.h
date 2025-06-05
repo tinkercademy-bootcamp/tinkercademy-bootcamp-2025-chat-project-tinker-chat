e3e819305995ba3986c9097d66af9b0a8e5d1f59
Rishika Garg <user@ip-172-31-18-95.ap-south-1.compute.internal>
2025-06-05 04:30:38 +0000

    Refactor- Add comments in header file

--- exercise-7/src/server/commands/command-handler.h
+++ exercise-7/src/server/commands/command-handler.h
@@ -11,19 +11,89 @@
 namespace tt::chat::server::commands {
 
 // Dispatcher
+/*
+Function: handle_command
+Purpose: Handles commands sent by the client.
+Parameters:
+  client - The ClientInfo object containing client details.
+  tokens - A vector of strings containing the command and its arguments.
+  channels - A map of channel names to sets of client file descriptors.
+Returns: None
+Throws: None
+*/
 void handle_command(ClientInfo& client,
                     std::vector<std::string>& tokens,
                     std::unordered_map<std::string, std::set<int>>& channels);
 
 // Individual command functions
+/*
+Function: command_username
+Purpose: Sets the username for the client.
+Parameters:
+  client - The ClientInfo object containing client details.
+  tokens - A vector of strings containing the command and its arguments.
+Returns: None
+Throws: None
+*/
 void command_username(ClientInfo& client, const std::vector<std::string>& tokens);
+
+/*
+Function: command_join
+Purpose: Joins a channel for the client.
+Parameters:
+  client - The ClientInfo object containing client details.
+  tokens - A vector of strings containing the command and its arguments.
+  channels - A map of channel names to sets of client file descriptors.
+Returns: None
+Throws: None
+*/
 void command_join(ClientInfo& client, const std::vector<std::string>& tokens,
                   std::unordered_map<std::string, std::set<int>>& channels);
+
+/*
+Function: command_leave
+Purpose: Leaves the current channel for the client.
+Parameters:
+  client - The ClientInfo object containing client details.
+  channels - A map of channel names to sets of client file descriptors.
+Returns: None
+Throws: None
+*/
 void command_leave(ClientInfo& client,
                    std::unordered_map<std::string, std::set<int>>& channels);
+
+/*
+Function: command_list
+Purpose: Lists all available channels and their members.
+Parameters:
+  fd - The file descriptor of the client to whom the list will be sent.
+  channels - A map of channel names to sets of client file descriptors.
+Returns: None
+Throws: None
+*/
 void command_list(int fd, const std::unordered_map<std::string, std::set<int>>& channels);
+
+/*
+Function: command_create
+Purpose: Creates a new channel with the given name.
+Parameters:
+  client - The ClientInfo object containing client details.
+  tokens - A vector of strings containing the command and its arguments.
+  channels - A map of channel names to sets of client file descriptors.
+Returns: None
+Throws: None
+*/
 void command_create(ClientInfo& client, const std::vector<std::string>& tokens,
                     std::unordered_map<std::string, std::set<int>>& channels);
+
+/*
+Function: command_exit
+Purpose: Exits the chat server for the client.
+Parameters:
+  client - The ClientInfo object containing client details.
+Returns: None
+Throws: None
+*/
 void command_exit(ClientInfo& client);
 
 } // namespace tt::chat::server
--- exercise-7/src/server/commands/server-utils.h
+++ exercise-7/src/server/commands/server-utils.h
@@ -10,11 +10,61 @@
 
 namespace tt::chat::server::commands {
 
+/*
+Function: send_message
+Purpose: Sends a message to a client.
+Parameters:
+  fd - The file descriptor of the client to whom the message will be sent.
+  msg - The message to be sent.
+Returns: None
+Throws: None
+*/
 void send_message(int fd, const std::string& msg);
+
+/*
+Function: is_valid_args
+Purpose: Checks if the number of arguments in a command is valid.
+Parameters:
+  tokens - A vector of strings containing the command and its arguments.
+  expected_count - The expected number of arguments (including the command itself).
+Returns: true if the number of arguments matches the expected count, false otherwise.
+Throws: None
+*/
 bool is_valid_args(const std::vector<std::string>& tokens, size_t expected_count);
+
+/*
+Function: channel_exists
+Purpose: Checks if a channel with the given name exists.
+Parameters:
+  name - The name of the channel to check.
+  channels - A map of channel names to sets of client file descriptors.
+Returns: true if the channel exists, false otherwise.
+Throws: None
+*/
 bool channel_exists(const std::string& name, const std::unordered_map<std::string, std::set<int>>& channels);
+
+/*
+Function: move_client_to_channel
+Purpose: Moves a client to a new channel.
+Parameters:
+  client - The ClientInfo object containing client details.
+  new_channel - The name of the new channel to which the client will be moved.
+  channels - A map of channel names to sets of client file descriptors.
+Returns: None
+Throws: None
+*/
 void move_client_to_channel(struct ClientInfo& client, const std::string& new_channel,
                             std::unordered_map<std::string, std::set<int>>& channels);
+
+/*
+Function: try_create_channel
+Purpose: Attempts to create a new channel with the given name.
+Parameters:
+  name - The name of the channel to be created.
+  channels - A map of channel names to sets of client file descriptors.
+Returns: true if the channel was successfully created, false if it already exists.
+Throws: None
+*/
 bool try_create_channel(const std::string& name,
                         std::unordered_map<std::string, std::set<int>>& channels);
 
