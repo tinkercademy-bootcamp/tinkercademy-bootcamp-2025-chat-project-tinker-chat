5f97710ec26edf482db2e117d9f22101d4ecfe7e
Rishika Garg <user@ip-172-31-18-95.ap-south-1.compute.internal>
2025-06-05 08:29:02 +0000

    Feat- Make client to write their username using argument while running the code

--- exercise-7/src/client-main.cc
+++ exercise-7/src/client-main.cc
@@ -5,14 +5,21 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <iostream>
 
 #include "client/chat-client.h"
 
-int main() {
+int main(int argc, char *argv[]) {
   const int kPort = 8080;
   const std::string kServerAddress = "127.0.0.1";
 
-  tt::chat::client::Client client{kPort, kServerAddress};
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <username>\n";
+    return EXIT_FAILURE;
+  }
+  std::string username = argv[1];
+
+  tt::chat::client::Client client{kPort, kServerAddress, username};
 
   client.start();
 
